7cef0ec91a73af783f56dccc0140248a2bd4726c
Jens-Christian Fischer <jens-christian.fischer@switch.ch>
2013-07-15 16:02:02 +0200

    disable raise for now

--- manifests/endpoint.pp
+++ manifests/endpoint.pp
@@ -31,7 +31,7 @@ class keystone::endpoint(
 ) {
   if $internal_port == undef {
     if $public_protocol == 'https' {
-      fail('you must set internal_port if using https')
+      # fail('you must set internal_port if using https')
     }
     $real_internal_port = $public_port
   } else {
