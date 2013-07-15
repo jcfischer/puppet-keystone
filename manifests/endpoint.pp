a8e86f57b6307f808848ae3bc81312ed61566d21
Jens-Christian Fischer <jens-christian.fischer@switch.ch>
2013-07-15 15:17:20 +0200

    check for set internal_port

--- manifests/endpoint.pp
+++ manifests/endpoint.pp
@@ -29,8 +29,10 @@ class keystone::endpoint(
   $region           = 'RegionOne',
   $public_protocol  = 'http'
 ) {
-
   if $internal_port == undef {
+    if $public_protocol == 'https' {
+      fail('you must set internal_port if using https')
+    }
     $real_internal_port = $public_port
   } else {
     $real_internal_port = $internal_port
